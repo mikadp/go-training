b5b37d824b29a38e9b5d7cd76c58e9fccabb324e
mikadp <37142855+mikadp@users.noreply.github.com>
2021-12-30 16:36:36 +0200

    commit go-training

--- learning.go
+++ learning.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("test)
+func main() {
+	fmt.Println("test")
+
+	var aString string = "This is test"
+	fmt.Println(aString)
+	fmt.Printf("The variable's type is %T\n", aString)
+
+	var anInteger int = 50
+	fmt.Println(anInteger)
+	fmt.Printf("the variable's type is %T\n", anInteger)
+
 }
